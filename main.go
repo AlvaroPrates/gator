2dbb9153123a6ff7b809a756abb02bc25f8d59b1
AlvaroPrates <alvarobirckprates@gmail.com>
2024-10-16 11:35:16 -0300

    add login command

--- main.go
+++ main.go
@@ -1,27 +1,44 @@
 package main
 
 import (
-	"fmt"
 	"log"
+	"os"
 
 	"github.com/AlvaroPrates/gator/internal/config"
 )
+
+type state struct {
+	cfg *config.Config
+}
 
 func main() {
 	cfg, err := config.Read()
 	if err != nil {
 		log.Fatalf("error reading config: %v", err)
 	}
-	fmt.Printf("Read config: %+v\n", cfg)
 
-	if err := cfg.SetUser("Álvaro"); err != nil {
+	progState := state{
+		cfg: &cfg,
+	}
+
+	cmds := commands{
+		list: make(map[string]func(*state, command) error),
+	}
+	cmds.register("login", handlerLogin)
+
+	if len(os.Args) < 2 {
+		log.Fatal("Usage: cli <command> [args...]")
+	}
+
+	cmdName := os.Args[1]
+	cmdArgs := os.Args[2:]
+
+	cmd := command{
+		Name: cmdName,
+		Args: cmdArgs,
+	}
+
+	if err = cmds.run(&progState, cmd); err != nil {
 		log.Fatal(err)
-		return
 	}
-
-	cfg, err = config.Read()
-	if err != nil {
-		log.Fatalf("error reading config: %v", err)
-	}
-	fmt.Printf("Read config again: %+v\n", cfg)
 }
